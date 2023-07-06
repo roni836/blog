1af1f7858cc508407665cfe83fbb6f1c22988b0d
roni836 <ronitsaha836@gmail.com>
2023-07-06 20:23:06 +0530

    first commit

---
+++ cms/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from .models import *
+
+admin.site.register(Category)
+
+class PostAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug":["title"]}
+
+
+admin.site.register(Post,PostAdmin)
